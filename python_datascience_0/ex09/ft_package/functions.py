7638c2d21ad8617ef2b094dcea870c3640b1b428
Paco Rocha <frocha@f-rocha.com>
2024-05-07 23:38:16 -0300

    finished

---
+++ python_datascience_0/ex09/ft_package/functions.py
@@ -0,0 +1,12 @@
+def count_in_list(list_, item):
+    """
+    Counts the ocurrences of an item in a list.
+
+    Parameters:
+        list_ (list): The list to search in.
+        item: The item to count occurrences of.
+
+    Returns:
+        int: The number of occurrences of the item in the list.
+    """
+    return list_.count(item)
