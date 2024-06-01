53b1998a748d7a1caa11dc3749e42535d45d9243
Paco Rocha <frocha@f-rocha.com>
2024-06-01 20:10:55 -0300

    finished ex05

---
+++ python_datascience_1/ex05/load_image.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import numpy as np
+
+np.set_printoptions(threshold=1)
+
+
+def ft_load(path: str) -> np.ndarray:
+    """
+    Load an image from the specified file path and convert it to a NumPy array.
+
+    This function opens an image file, prints its format and shape, and returns
+    the image data as a NumPy array. If the image cannot be loaded, an error
+    message is printed.
+
+    Args:
+        path (str): The file path to the image.
+
+    Returns:
+        np.ndarray: The image data as a NumPy array formatted for the required
+        output.
+
+    Raises:
+        Exception: If there is an error in loading the image, the exception is
+        caught and an error message is printed.
+    """
+    try:
+        with Image.open(path) as img:
+            print(f"The format of the image is: {img.format}")
+
+            img_array = np.array(img)
+
+            print(f"The shape of the image is: {img_array.shape}")
+
+            first_row_elements = img_array[0, :3]
+            last_row_elements = img_array[-1, -3:]
+
+            print(f"[{ first_row_elements }\n    ...\n{last_row_elements}]")
+
+            return img_array
+
+    except Exception as e:
+        print(f"Error loading image: {e}")
---
+++ python_datascience_1/ex05/pimp_image.py
@@ -0,0 +1,56 @@
+from array import array
+from PIL import Image
+import numpy as np
+
+
+def display_image(array, title):
+    img = Image.fromarray(array)
+    img.show(title=title)
+
+
+def ft_invert(array) -> array:
+    inverted_array = 255 - array
+    title = "inverted"
+    display_image(inverted_array, title)
+
+    return array
+
+
+def ft_red(array) -> array:
+    red_array = array.copy()
+    red_array[:, :, [1, 2]] = 0
+    title = "red"
+    display_image(red_array, title)
+
+    return array
+
+
+def ft_green(array) -> array:
+    green_array = array.copy()
+    green_array[:, :, [0, 2]] = 0
+    title = "green"
+    display_image(green_array, title)
+
+    return array
+
+
+def ft_blue(array) -> array:
+    blue_array = array.copy()
+    blue_array[:, :, [0, 1]] = 0
+    title = "blue"
+    display_image(blue_array, title)
+
+    return array
+
+
+def ft_grey(array) -> array:
+    grey_array = array.copy()
+    grey = (grey_array[:, :, 0] / 3 + grey_array[:, :, 1] / 3 + grey_array[:, :, 2] / 3).astype(np.uint8)
+    grey_array[:, :, 0] = grey
+    grey_array[:, :, 1] = grey
+    grey_array[:, :, 2] = grey
+
+    title = "grey"
+    display_image(grey_array, title)
+
+    return array
