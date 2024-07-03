d294d4fac03d5a23bb6166175573d1e08fcb6f14
Paco Rocha <frocha@f-rocha.com>
2024-07-02 22:40:02 -0300

    added exceptions to ex03

--- python_datascience_2/ex03/projection_life.py
+++ python_datascience_2/ex03/projection_life.py
@@ -5,19 +5,32 @@ from load_csv import load
 
 
 def main(argv):
-    file1 = "./income_per_person_gdppercapita_ppp_inflation_adjusted.csv"
-    file2 = "./life_expectancy_years.csv"
-    year = argv[1]
-    df_file1 = load(file1)
-    df_file2 = load(file2)
-    year_income = df_file1[year]
-    life_expectancy_data = df_file2[year]
+    try:
+        if len(argv) < 2:
+            raise ValueError("A year must be specified as an argument.")
 
-    plt.title(f'{year}')
-    plt.xlabel('Gross domestic product')
-    plt.ylabel('Life Expectancy')
-    plt.scatter(year_income, life_expectancy_data)
-    plt.show()
+        file1 = "./income_per_person_gdppercapita_ppp_inflation_adjusted.csv"
+        file2 = "./life_expectancy_years.csv"
+        year = argv[1]
+
+        df_file1 = load(file1)
+        df_file2 = load(file2)
+        year_income = df_file1[year]
+        life_expectancy_data = df_file2[year]
+
+        plt.title(f'{year}')
+        plt.xlabel('Gross domestic product')
+        plt.ylabel('Life Expectancy')
+        plt.scatter(year_income, life_expectancy_data)
+        plt.show()
+
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+    except KeyError as ke:
+        print(f"KeyError: {ke}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
 if __name__ == "__main__":
     main(sys.argv)
