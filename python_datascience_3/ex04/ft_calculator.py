9b1089f3e12769ce2c1da7f404c21566fe268d3f
pacorocha <frocha@f-rocha.com>
2024-08-15 18:44:23 -0300

    list 3 ok

---
+++ python_datascience_3/ex04/ft_calculator.py
@@ -0,0 +1,19 @@
+class calculator:
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        product_list = []
+        for i in range(len(V1)):
+            product_list.append(V1[i] * V2[i])
+        print("Dot product is: ", sum(product_list))
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        total = []
+        for i in range(len(V1)):
+            total.append(float(V1[i] + V2[i]))
+        print("Add Vector is: ", total)
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        total = []
+        for i in range(len(V1)):
+            total.append(float(V1[i] - V2[i]))
+        print("Add Vector is: ", total)
