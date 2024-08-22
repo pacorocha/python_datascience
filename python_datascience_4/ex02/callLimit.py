2ac8817f20a295f70aa79da1c90f03bd6181ba43
pacorocha <frocha@f-rocha.com>
2024-08-22 15:25:53 -0300

    list 4 ex03 finished

--- python_datascience_4/ex02/callLimit.py
+++ python_datascience_4/ex02/callLimit.py
@@ -2,18 +2,31 @@ from typing import Any
 
 
 def callLimit(limit: int):
+    """
+    A decorator generator that limits the number of times a function can be
+    called.
+
+    Args:
+        limit (int): The maximum number of times the decorated function can be
+        called.
+
+    Returns:
+        Callable: A decorator that, when applied to a function, restricts its
+        usage to the specified number of calls. If the function is called more
+        than the allowed limit, an error message is printed, and the function
+        is not executed for the excess calls.
+    """
     count = 0
-    print("callLimit")
+
     def callLimiter(function):
         nonlocal count
 
-        print("callLimiter")
         def limit_function(*args: Any, **kwds: Any):
             nonlocal count
 
             count += 1
             if count > limit:
-                print("Function called too many times")
+                print(f"Error: {function} Function called too many times")
             else:
                 return function(*args, **kwds)
 
--- python_datascience_4/ex02/tester.py
+++ python_datascience_4/ex02/tester.py
@@ -1,13 +1,16 @@
 from callLimit import callLimit
 
+
 @callLimit(3)
 def f():
-    print ("f()")
+    print("f()")
+
 
 @callLimit(1)
 def g():
-    print ("g()")
+    print("g()")
 
-    for i in range(3):
-        f()
-        g()
+
+for i in range(3):
+    f()
+    g()
